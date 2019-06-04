944badc5d59a662030064ffad86553c6fd7c9917
curegit <curegit@users.noreply.github.com>
2019-06-04 18:52:12 +0900

    GameManagerにコメントを入れる

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,73 +7,73 @@ using ReversiTutorial.Model;
 namespace ReversiTutorial
 {
 	/// <summary>
-	/// 
+	/// ゲームの進行管理
 	/// </summary>
 	public class GameManager : MonoBehaviour
 	{
 		/// <summary>
-		/// 
+		/// 石のオブジェクト
 		/// </summary>
 		[SerializeField]
 		private GameObject disk;
 
 		/// <summary>
-		/// 
+		/// すべてのタイル
 		/// </summary>
 		[SerializeField]
 		private Square[] squares;
 
 		/// <summary>
-		/// （ゲーム途中での変更不可）
+		/// 黒にAIを使うか（ゲーム途中での変更不可）
 		/// </summary>
 		[SerializeField]
 		private bool useAIDark;
 
 		/// <summary>
-		/// 
+		/// 白にAIを使うか（ゲーム途中での変更不可）
 		/// </summary>
 		[SerializeField]
 		private bool useAILight;
 
 		/// <summary>
-		/// 
+		/// AIの最低思考時間
 		/// </summary>
 		[SerializeField, Range(0, 5000)]
 		private int minAIDelayMs = 1000;
 
 		/// <summary>
-		/// 
+		/// ゲームマネージャのシングルトン
 		/// </summary>
 		public static GameManager instance { get; private set; }
 
 		/// <summary>
-		/// 
+		/// ゲームモードを指定するための静的変数
 		/// </summary>
 		public static int newGameMode { get; set; }
 
 		/// <summary>
-		/// 
+		/// 盤の状態を表すオブジェクト
 		/// </summary>
 		public Board board { get; private set; }
 
 		/// <summary>
-		/// 
+		/// 石の場所からインスタンスを引く辞書
 		/// </summary>
 		private Dictionary<Position, Disk> disks = new Dictionary<Position, Disk>();
 
 		/// <summary>
-		/// 
+		/// AIが思考中かどうか
 		/// </summary>
 		private bool thinking;
 
 		/// <summary>
-		/// 
+		/// 初期化時に呼ばれる
 		/// </summary>
 		public void Awake()
 		{
-			// 
 			if (instance)
 			{
+				// シングルトンの初期化違反
 				Debug.LogWarning("GameManager already initialized");
 			}
 			else
@@ -83,11 +83,11 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 初期化時に呼ばれる
 		/// </summary>
 		private void Start()
 		{
-			// 
+			// 空の盤をつくる
 			board = new Board();
 			// 初期配置のとおりに石のゲームオブジェクトを置く
 			foreach (var dark in board.DiskPositions(Player.Dark))
@@ -103,14 +103,12 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 毎フレームの処理
 		/// </summary>
 		private void Update()
 		{
-			//
 			if (board.turn == Player.Dark && useAIDark || board.turn == Player.Light && useAILight)
 			{
-				//
 				if (board.CanMove() && !thinking)
 				{
 					thinking = true;
@@ -120,24 +118,18 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 現在のプレイヤーが特定の場所に石を置く
 		/// </summary>
-		/// <param name="x"></param>
-		/// <param name="z"></param>
+		/// <param name="x">x方向の区画座標</param>
+		/// <param name="z">z方向の区画座標</param>
 		public void Place(int x, int z)
 		{
-
 			if (board.CanPlace(x, z))
 			{
-
 				var side = board.turn;
 				var turnovers = board.GetTurnovers(x, z);
-
-				// 
 				board = board.Place(x, z);
-				// 
 				PlaceDiskObject(x, z, side);
-				// 
 				foreach (var turnover in turnovers)
 				{
 					disks[turnover].Flip(side);
@@ -146,7 +138,7 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 現在のプレイヤーがパスする
 		/// </summary>
 		public void Pass()
 		{
@@ -156,7 +148,12 @@ namespace ReversiTutorial
 			}
 		}
 
-
+		/// <summary>
+		/// 特定のプレイヤーの石を新しく配置する
+		/// </summary>
+		/// <param name="x">x方向の区画座標</param>
+		/// <param name="z">z方向の区画座標</param>
+		/// <param name="side">プレイヤー</param>
 		private void PlaceDiskObject(int x, int z, Player side)
 		{
 			var square = squares.Single(s => x == s.x && z == s.z);
@@ -168,10 +165,10 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 現在のプレイヤーがマスを選択ときに呼ばれる
 		/// </summary>
-		/// <param name="x"></param>
-		/// <param name="z"></param>
+		/// <param name="x">x方向の区画座標</param>
+		/// <param name="z">z方向の区画座標</param>
 		public void PlayerClick(int x, int z)
 		{
 			if (board.turn == Player.Dark && !useAIDark || board.turn == Player.Light && !useAILight)
@@ -181,7 +178,7 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// 現在のプレイヤーがパスを選んだときに呼ばれる
 		/// </summary>
 		public void PlayerPass()
 		{
@@ -192,9 +189,8 @@ namespace ReversiTutorial
 		}
 
 		/// <summary>
-		/// 
+		/// AIに思考させておく
 		/// </summary>
-		/// <returns></returns>
 		public async void LetAIThink()
 		{
 			if (board.IsEnd())
