dc1bd72d4137a1934d8ed39333a9ea844c6acb32
curegit <curegit@users.noreply.github.com>
2019-06-04 16:53:17 +0900

    タイトルスクリプトにコメントを入れる

--- Assets/Completed/Scripts/Title.cs
+++ Assets/Completed/Scripts/Title.cs
@@ -3,17 +3,26 @@ using UnityEngine.SceneManagement;
 
 namespace ReversiTutorial.Completed
 {
+	/// <summary>
+	/// タイトルシーンの制御
+	/// </summary>
 	public class Title : MonoBehaviour
 	{
-
+		/// <summary>
+		/// ゲームモードが選ばれたとき
+		/// </summary>
+		/// <param name="mode">ゲームモード</param>
 		public void OnPlayButtonClicked(int mode)
 		{
-			//
+			// ゲームモードを静的変数に入れておく
 			GameManager.newGameMode = mode;
-			//
+			// ゲームシーンをロード
 			SceneManager.LoadScene(1);
 		}
 
+		/// <summary>
+		/// ゲームプログラムの終了ボタンが押されたとき
+		/// </summary>
 		public void OnQuitButtonClicked()
 		{
 			Application.Quit();
